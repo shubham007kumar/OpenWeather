c76e2019176f359afa94e2205ec8905e4b2414ef
shubham kumar <yuvraj.shubham.ss@gmail.com>
2024-09-24 11:35:37 +0530

    add page for header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Header.module.css";
+export default function Header() {
+  return (
+    <div className={styles["container"]}>
+      <h1 className={styles["title"]}>OpenWeather</h1>
+      <h1 className={styles["sub-title"]}>
+        Weather forecasts, nowcasts and history in a fast and elegant way
+      </h1>
+    </div>
+  );
+}
